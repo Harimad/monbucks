d03bb43a7ac2990f5f32003b648dd3f3a10cff5e
Harimad <leesh68481@gmail.com>
2022-03-17 12:20:34 +0900

    9. 메뉴 추가하기 - 빈 값 입력에 대한 대응 | 예외 처리

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,9 @@
 // - [ ] 에스프레소 메뉴에 새로운 메뉴를 확인 버튼을 누르면 메뉴가 추가된다.
 // - [x] 에스프레소 메뉴에 새로운 메뉴를 엔터키 입력으로 메뉴가 추가된다.
 // - [x] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
-// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
-// - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
 const $ = selector => document.querySelector(selector)
 
@@ -19,7 +19,16 @@ function App() {
 
   // 메뉴의 이름을 입력받는건
   $('#espresso-menu-name').addEventListener('keydown', e => {
-    if (e.key == 'Enter' && $('#espresso-menu-name').value) {
+    if (e.key !== 'Enter') {
+      return
+    }
+
+    if ($('#espresso-menu-name').value === '') {
+      alert('값을 입력해주세요')
+      return
+    }
+
+    if (e.key == 'Enter') {
       const espressoMenuName = $('#espresso-menu-name').value
       const menuItemTemplate = espressoMenuName => {
         return `<li class="menu-list-item d-flex items-center py-2">
