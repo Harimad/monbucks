5d3a4d15df811ea852b4b5a82670eba075370fad
Harimad <leesh68481@gmail.com>
2022-03-17 12:04:04 +0900

    8. 메뉴 추가하기 - 카운트 갯수 업데이트 | DOM 핸들링

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@
 
 // TODO 메뉴 추가
 // - [ ] 에스프레소 메뉴에 새로운 메뉴를 확인 버튼을 누르면 메뉴가 추가된다.
-// - [ ] 에스프레소 메뉴에 새로운 메뉴를 엔터키 입력으로 메뉴가 추가된다.
-// - [ ] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
+// - [x] 에스프레소 메뉴에 새로운 메뉴를 엔터키 입력으로 메뉴가 추가된다.
+// - [x] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
 // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
@@ -19,7 +19,7 @@ function App() {
 
   // 메뉴의 이름을 입력받는건
   $('#espresso-menu-name').addEventListener('keydown', e => {
-    if (e.key == 'Enter') {
+    if (e.key == 'Enter' && $('#espresso-menu-name').value) {
       const espressoMenuName = $('#espresso-menu-name').value
       const menuItemTemplate = espressoMenuName => {
         return `<li class="menu-list-item d-flex items-center py-2">
@@ -43,6 +43,12 @@ function App() {
         'beforeend',
         menuItemTemplate(espressoMenuName)
       )
+
+      const menuCount = $('#espresso-menu-list').querySelectorAll('li').length
+      $('.menu-count').innerText = `
+			총 ${menuCount}개`
+
+      $('#espresso-menu-name').value = ''
     }
   })
 }
