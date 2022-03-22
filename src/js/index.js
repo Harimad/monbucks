ef046266dd9162bb541e75aeec4c4ae493140c8a
Harimad <leesh68481@gmail.com>
2022-03-22 13:44:38 +0900

    40. 리팩터링 - API 호출 객체 추상화

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,7 @@ import MenuApi from './api/index.js'
 // [x] fetch 비동기 api를 사용하는 부분을 async await을 사용하여 구현한다.
 
 // TODO 사용자 경험
-// [x] API 통신이 실패하는 경우에 대해 사용자가 알 수 있게 alert으로 예외처리를 진행한다.
+// [X] API 통신이 실패하는 경우에 대해 사용자가 알 수 있게 alert으로 예외처리를 진행한다.
 // [x] 중복되는 메뉴는 추가할 수 없다.
 
 function App() {
